50d6d0e935ad4ec7909ca388ef0627cefb1b3894
Jospeh <ejosephvito@gmail.com>
2019-06-29 20:11:20 -0400

    basics of combat js

---
+++ js/combat.js
@@ -0,0 +1,15 @@
+// Determine who goes first
+
+// Take first player's turrn
+
+// Draw card from library
+
+// Tick down ALL cards in waiting
+
+// Play ready cards
+
+// Attack
+
+// Loop through switching active player
+
+// Exit on victory
