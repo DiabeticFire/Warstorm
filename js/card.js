c759426ba898dd9bd2d09176db8349fa805339d1
Jospeh <ejosephvito@gmail.com>
2019-07-06 10:17:48 -0400

    small updates

---
+++ js/card.js
@@ -0,0 +1,9 @@
+class Card {
+  constructor(name, faction, type, ready, power) {
+    this.name = name;
+    this.faction = faction;
+    this.type = type;
+    this.ready = ready;
+    this.power = power;
+  }
+}
